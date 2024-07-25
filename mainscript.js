78390b1c842b303893652afa9691cb02638ec2ce
Andy Develop <formytikidea@gmail.com>
2024-07-25 06:21:53 +0400

    Create mainscript.js

---
+++ mainscript.js
@@ -0,0 +1,8 @@
+I have Auto Withdraw/sweeper bot STOPPING/REMOVE SCRIPT for all chain and All tokens , TRX/SOL/BNB/BSC/ETH and others FLASHBOTS
+
+for more information you can text me in TG - https://t.me/Andy_develop
+
+https://t.me/Andy_develop
+https://t.me/Andy_develop
+
+TG HERE --- @andy_Develop
